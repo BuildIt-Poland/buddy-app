367fc7dc5e98cfa2c28a2a32dfb1e903cc0d0f7d
Tonik1204 <las12041991@gmail.com>
2019-10-01 21:28:32 +0200

    [BUD-65] Add GraphQL Apollo Client + enzyme test framework

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { MockedProvider } from '@apollo/react-testing';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <MockedProvider mocks={[]} addTypename={false}>
+      <App />
+    </MockedProvider>,
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient, ApolloLink, InMemoryCache, HttpLink } from 'apollo-boost';
 import * as serviceWorker from './serviceWorker';
+import './index.css';
+import App, { AUTH_TOKEN } from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const httpLink = new HttpLink({ uri: 'http://localhost:4000' });
 
+const authLink = new ApolloLink((operation, forward) => {
+  // Retrieve the authorization token from local storage.
+  const token = localStorage.getItem(AUTH_TOKEN);
+
+  // Use the setContext method to set the HTTP headers.
+  operation.setContext({
+    headers: {
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  });
+
+  // Call the next link in the middleware chain.
+  return forward(operation);
+});
+
+export const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
