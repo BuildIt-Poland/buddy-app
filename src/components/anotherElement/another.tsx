a07efb5e61e7de72b478920c3145a38ea51d78f0
Luciano Santos <luciusjss@gmail.com>
2019-10-04 17:26:08 +0200

    remove dummy component for test purposes

--- src/components/anotherElement/another.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Typography from '@material-ui/core/Typography';
-
-const teste: React.FC = () => {
-  return <Typography>Buddy teste</Typography>;
-};
-
-export default teste;
