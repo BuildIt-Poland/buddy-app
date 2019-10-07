5a42a9c6d67c910e4907cd9a90bf35f5d0af2d32
Tonik1204 <las12041991@gmail.com>
2019-10-07 17:08:05 +0200

    [BUD-107] Add readme description

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -1,5 +1,5 @@
 const cypressTypeScriptPreprocessor = require('./cy-ts-preprocessor');
 
-module.exports = on => {
+module.exports = (on: any) => {
   on('file:preprocessor', cypressTypeScriptPreprocessor);
 };
