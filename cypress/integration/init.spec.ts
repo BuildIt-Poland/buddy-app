9a2e53eb94ebf12f3be587c684d418e119d8b2e7
Tonik1204 <las12041991@gmail.com>
2019-10-07 15:25:19 +0200

    [BUD-107] Add Cypress to the project

---
+++ cypress/integration/init.spec.ts
@@ -0,0 +1,8 @@
+describe('Buddy App Test', () => {
+  it('shows root component', function() {
+    cy.visit('/');
+    cy.get('.MuiTypography-root')
+      .should('be.visible')
+      .and('have.text', 'Buddy App');
+  });
+});
