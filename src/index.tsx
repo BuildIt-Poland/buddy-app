5056d6955e21bf66037b5a0dee87594d47dd867d
Tonik1204 <las12041991@gmail.com>
2019-10-08 15:02:29 +0200

    [BUD-65] Add utils/apollo-client

--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ApolloProvider } from '@apollo/react-hooks';
-import { ApolloClient, ApolloLink, InMemoryCache, HttpLink } from 'apollo-boost';
+import client from './utils/apollo-client';
 import * as serviceWorker from './serviceWorker';
 import App from './components/App';
-
-const httpLink = new HttpLink({ uri: process.env.REACT_APP_SERVER_URL });
-
-const authLink = new ApolloLink((operation, forward) => {
-  const token = localStorage.getItem(process.env.REACT_APP_AUTH_TOKEN || '');
-
-  operation.setContext({
-    headers: {
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  });
-
-  return forward(operation);
-});
-
-export const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
 
 ReactDOM.render(
   <ApolloProvider client={client}>
