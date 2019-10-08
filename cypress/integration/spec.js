02110c4edb125bfa9de535c17d8a0b9b9180c9fd
Tonik1204 <las12041991@gmail.com>
2019-10-08 08:50:38 +0200

    [BUD-107] remove types from cypress

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,9 +1,7 @@
-/// <reference types="cypress" />
-
 describe('Buddy App Test', () => {
   it('shows root component', function() {
     cy.visit('/');
-    cy.get('.MuiTypography-root')
+    cy.dataCy('root')
       .should('be.visible')
       .and('have.text', 'Buddy App');
   });
--- cypress/plugins/index.ts
+++
@@ -1,5 +0,0 @@
-const cypressTypeScriptPreprocessor = require('./cy-ts-preprocessor');
-
-module.exports = (on: any) => {
-  on('file:preprocessor', cypressTypeScriptPreprocessor);
-};
