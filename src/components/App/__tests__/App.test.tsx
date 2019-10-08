378c78cdcbec45ae442ffdfc34a22bbb9a06c90f
Luciano Santos <luciusjss@gmail.com>
2019-10-08 15:25:56 +0200

    Update snapshot test in App

--- src/components/App/__tests__/App.test.tsx
+++ src/components/App/__tests__/App.test.tsx
@@ -5,7 +5,6 @@ import App from '../App';
 
 jest.mock('@material-ui/core/CssBaseline', () => 'CssBaseline');
 jest.mock('@material-ui/styles/ThemeProvider', () => 'ThemeProvider');
-jest.mock('@material-ui/core/Typography', () => 'Typography');
 jest.mock('../../../styles/theme.ts', () => {});
 jest.mock('../../AppWrapper', () => 'AppWrapper');
 
