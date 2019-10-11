41d02dfcfba61a7b1e35e5934470d5d67e3a6eeb
Luciano Santos <luciusjss@gmail.com>
2019-10-11 16:33:19 +0200

    TextField and typography added to Login screen

--- src/components/Login/__tests__/Login.spec.tsx
+++ src/components/Login/__tests__/Login.spec.tsx
@@ -3,6 +3,10 @@ import { create } from 'react-test-renderer';
 
 import Login from '../Login';
 jest.mock('@material-ui/core/Typography', () => 'Typography');
+jest.mock('@material-ui/core/Grid', () => 'Grid');
+jest.mock('@material-ui/core/Link', () => 'Link');
+jest.mock('@material-ui/core/TextField', () => 'TextField');
+
 jest.mock('../../RoundedButton/', () => 'RoundedButton');
 
 describe('Component - Login', () => {
