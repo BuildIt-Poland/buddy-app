21f1f60fa6372faef1348c278548dd3c3188e32e
Luciano Santos <luciusjss@gmail.com>
2019-10-11 13:45:03 +0200

    Merged with master

--- src/components/Login/__tests__/Login.spec.tsx
+++ src/components/Login/__tests__/Login.spec.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { create } from 'react-test-renderer';
 
 import Login from '../Login';
+jest.mock('@material-ui/core/Typography', () => 'Typography');
+jest.mock('../../RoundedButton/', () => 'RoundedButton');
 
 describe('Component - Login', () => {
   test('renders correctly', () => {
