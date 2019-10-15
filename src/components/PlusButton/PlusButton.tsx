2990819f56621f4cca72c7ad72d249e207580ee0
Tonik1204 <las12041991@gmail.com>
2019-10-15 14:54:02 +0200

    [BUD-92] Add material-ui icon instead of symbol

--- src/components/PlusButton/PlusButton.tsx
+++ src/components/PlusButton/PlusButton.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import Fab, { FabProps } from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
 
-const PlusButton = (props: FabProps) => <Fab {...props}>+</Fab>;
+const PlusButton = (props: FabProps) => (
+  <Fab {...props} aria-label='add'>
+    <AddIcon />
+  </Fab>
+);
 
 PlusButton.defaultProps = {
   color: 'primary',
--- src/components/PlusButton/__tests__/PlusButton.test.tsx
+++ src/components/PlusButton/__tests__/PlusButton.test.tsx
@@ -4,6 +4,7 @@ import { create } from 'react-test-renderer';
 import PlusButton from '../PlusButton';
 
 jest.mock('@material-ui/core/Fab', () => 'Fab');
+jest.mock('@material-ui/icons/Add', () => 'AddIcon');
 
 describe('Component - PlusButton', () => {
   test('renders correctly', () => {
