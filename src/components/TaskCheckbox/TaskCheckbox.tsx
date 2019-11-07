360e412e65ea3cad53a1e7e38f1e28a4fdfae35b
Michael Money <mpieniazek@me.com>
2019-11-07 11:25:52 +0100

    Use components from material-ui lib

--- src/components/TaskCheckbox/TaskCheckbox.tsx
+++ src/components/TaskCheckbox/TaskCheckbox.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { FormControlLabel } from '@material-ui/core';
+import Checkbox from '@material-ui/core/Checkbox';
 
-const TaskCheckbox: React.FC<any> = props => {
-  return <></>;
+const TaskCheckbox: React.FC<any> = ({ label, state, onChange }) => {
+  return (
+    <FormControlLabel
+      value={state}
+      control={<Checkbox color='primary' onChange={onChange} />}
+      label={label}
+      labelPlacement='end'
+    />
+  );
 };
 
 export default TaskCheckbox;
--- src/components/TasksList/TasksList.tsx
+++ src/components/TasksList/TasksList.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import TaskCheckbox from '../TaskCheckbox/TaskCheckbox';
 
 const TasksList: React.FC = () => {
   return (
     <>
       <h1>Tasks List</h1>
+      <TaskCheckbox label={'Change you line manager'} state={'todo'} />
     </>
   );
 };
