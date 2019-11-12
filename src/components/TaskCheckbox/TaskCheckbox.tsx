044e78787dfa2f14de4ae6eedd73f559b041c2d1
Michael Money <mpieniazek@me.com>
2019-11-12 14:36:58 +0100

    [BUD-91] Checkbox Component (#30)
    * Scaffold TaskCheckbox component
    
    * Use components from material-ui lib
    
    * Add styles and missing logic
    
    * Use rems instead pixels

---
+++ src/components/TaskCheckbox/TaskCheckbox.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import Checkbox from '@material-ui/core/Checkbox';
+import { Box, Typography } from '@material-ui/core';
+import makeStyles from '@material-ui/core/styles/makeStyles';
+import { Task } from '../../../server/src/generated/schema-types';
+import { TaskCheckBoxOwnProps, TaskStatus } from './types';
+
+const useStyles = makeStyles({
+  wrapper: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  titleWrapper: {
+    position: 'relative',
+  },
+  line: {
+    width: '100%',
+    position: 'absolute',
+    height: '0.1rem',
+    backgroundColor: 'rgba(0, 0, 0, 0.28)',
+    top: '1.2rem',
+  },
+});
+
+const TaskCheckbox: React.FC<Partial<Task> & TaskCheckBoxOwnProps> = ({
+  id,
+  title,
+  status,
+  onChange,
+}) => {
+  const { wrapper, line, titleWrapper } = useStyles();
+
+  const isChecked = (status: TaskStatus) => status === TaskStatus.Completed;
+
+  return (
+    <Box className={wrapper}>
+      <Checkbox
+        color='primary'
+        checked={isChecked(status as TaskStatus)}
+        onChange={() => onChange(id as string)}
+        value={id}
+        inputProps={{
+          'aria-label': 'primary checkbox',
+        }}
+      />
+      <Box className={titleWrapper}>
+        <Typography component='h4' variant='body1'>
+          <Box
+            fontWeight={
+              isChecked(status as TaskStatus)
+                ? 'fontWeightRegular'
+                : 'fontWeightBold'
+            }>
+            {title}
+          </Box>
+          {isChecked(status as TaskStatus) && <div className={line} />}
+        </Typography>
+      </Box>
+    </Box>
+  );
+};
+
+export default TaskCheckbox;
