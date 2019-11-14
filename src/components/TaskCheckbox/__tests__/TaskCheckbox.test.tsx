ebd360bf4cbf20bf0e84b0ed89f526d05dc06a8e
Tonik1204 <las12041991@gmail.com>
2019-11-14 23:43:08 +0100

    [BUD-202] Test updates

--- src/components/TaskCheckbox/__tests__/TaskCheckbox.test.tsx
+++ src/components/TaskCheckbox/__tests__/TaskCheckbox.test.tsx
@@ -1,30 +1,32 @@
 import React from 'react';
 import { create } from 'react-test-renderer';
-
-import { TaskStatus } from 'buddy-app-schema';
+import { mockSchemaTypes } from '__mocks__';
 import TaskCheckbox from '../TaskCheckbox';
 
+jest.mock('buddy-app-schema', () => mockSchemaTypes());
 jest.mock('@material-ui/core/Box', () => 'Box');
 jest.mock('@material-ui/core/Typography', () => 'Typography');
 jest.mock('@material-ui/core/Checkbox', () => 'Checkbox');
 
 describe('Component - TaskCheckbox', () => {
-  test(`renders correctly when task is ${TaskStatus.Uncompleted}`, () => {
+  const taskStatus = mockSchemaTypes().TaskStatus;
+
+  test(`renders correctly when task is ${taskStatus.Uncompleted}`, () => {
     const component = create(
       <TaskCheckbox
         title={'Change you line manager'}
-        status={TaskStatus.Uncompleted}
+        status={taskStatus.Uncompleted}
         id={'1'}
         onChange={() => null}
       />
     );
     expect(component.toJSON()).toMatchSnapshot();
   });
-  test(`renders correctly when task is ${TaskStatus.Completed}`, () => {
+  test(`renders correctly when task is ${taskStatus.Completed}`, () => {
     const component = create(
       <TaskCheckbox
         title={'Change you line manager'}
-        status={TaskStatus.Completed}
+        status={taskStatus.Completed}
         id={'1'}
         onChange={() => null}
       />
--- src/utils/__tests__/helpers.test.ts
+++ src/utils/__tests__/helpers.test.ts
@@ -1,14 +1,23 @@
 import { isNewbie } from 'utils';
-import { UserRole } from 'buddy-app-schema';
+import { mockSchemaTypes } from '__mocks__';
+
+jest.mock('buddy-app-schema', () => ({
+  UserRole: {
+    Newbie: 'NEWBIE',
+    Buddy: 'BUDDY',
+  },
+}));
 
 describe('Utils - helpers', () => {
+  const userRole = mockSchemaTypes().UserRole;
+
   it('helpers - isNewbie should return true', () => {
-    const result = isNewbie(UserRole.Newbie);
+    const result = isNewbie(userRole.Newbie);
     expect(result).toBe(true);
   });
 
   it('helpers - isNewbie should return false', () => {
-    const result = isNewbie(UserRole.Buddy);
+    const result = isNewbie(userRole.Buddy);
     expect(result).toBe(false);
   });
 });
