987fa0911bccd05aa5116d5a4029f4a8dbca4423
Michael Money <mpieniazek@me.com>
2019-11-14 15:05:33 +0100

    Fix NavBar mock and update snapshots

--- src/components/TasksList/__tests__/TasksList.test.tsx
+++ src/components/TasksList/__tests__/TasksList.test.tsx
@@ -4,7 +4,7 @@ import { create } from 'react-test-renderer';
 import TasksList from '../TasksList';
 
 jest.mock('components/AvatarHeader', () => 'AvatarHeader');
-jest.mock('components/Navbar', () => 'Navbar');
+jest.mock('components/NavBar', () => 'NavBar');
 
 describe('Component - TasksList', () => {
   test('renders correctly', () => {
