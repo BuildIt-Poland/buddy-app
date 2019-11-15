1b52029c81aa9a9b6759e6ef6c97bae39f288f6c
Luciano Santos <luciusjss@gmail.com>
2019-11-15 12:23:06 +0100

    TabPanel types file added

--- src/components/TasksList/TabPanel.tsx
+++ src/components/TasksList/TabPanel.tsx
@@ -1,13 +1,8 @@
 import React from 'react';
 import Box from '@material-ui/core/Box';
+import { TabPanelProps } from './types';
 
-interface TabPanelProps {
-  children?: React.ReactNode;
-  index: any;
-  value: any;
-}
-
-function TabPanel(props: TabPanelProps) {
+const TabPanel = (props: TabPanelProps) => {
   const { children, value, index, ...other } = props;
 
   return (
@@ -15,6 +10,6 @@ function TabPanel(props: TabPanelProps) {
       {children}
     </Box>
   );
-}
+};
 
 export default TabPanel;
