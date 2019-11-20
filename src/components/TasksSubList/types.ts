202414c215726f9fe9e9d0304fe5f963c29d5d37
Tonik1204 <las12041991@gmail.com>
2019-11-20 01:48:04 +0100

    [BUD-86] Add TasksSubList component

---
+++ src/components/TasksSubList/types.ts
@@ -0,0 +1,8 @@
+import { Task } from 'buddy-app-schema';
+import { CheckboxProps } from '../TaskCheckbox';
+
+export interface TasksSubListProps extends CheckboxProps {
+  tasks: Task[];
+  title: string;
+  count: number;
+}
