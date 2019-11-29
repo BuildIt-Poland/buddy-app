a8439ac88e5d555fcf39773151447ce5cf48a25a
Tonik1204 <las12041991@gmail.com>
2019-11-29 11:01:24 +0100

    [BUD-30] Add mocks to task details test

--- src/components/TaskDetails/__tests__/TaskDetails.test.tsx
+++ src/components/TaskDetails/__tests__/TaskDetails.test.tsx
@@ -7,6 +7,11 @@ import { taskDetailsMock } from '__mocks__';
 import TaskDetails from '../TaskDetails';
 
 jest.mock('components/AppWrapper', () => 'AppWrapper');
+jest.mock('components/NavBar', () => 'NavBar');
+jest.mock('@material-ui/core/CircularProgress', () => 'CircularProgress');
+jest.mock('components/TaskCheckbox', () => 'TaskCheckbox');
+jest.mock('@material-ui/core/Box', () => 'Box');
+jest.mock('@material-ui/core/Typography', () => 'Typography');
 jest.mock('decorators/withSnackBar', () => (component: React.FC<any>) => component);
 
 describe('Component - TaskDetails', () => {
