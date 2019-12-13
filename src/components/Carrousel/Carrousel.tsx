e8a421ab20d64550b911817f63b2754924292800
Tonik1204 <las12041991@gmail.com>
2019-12-13 19:55:51 +0100

    [BUD-213] Revert select newbies

--- src/components/Carrousel/Carrousel.tsx
+++ src/components/Carrousel/Carrousel.tsx
@@ -1,41 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 import Paper from '@material-ui/core/Paper';
-// import Typography from '@material-ui/core/Typography';
-// import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles, Theme } from '@material-ui/core/styles';
 import { Newbie } from 'buddy-app-schema';
 import { ROUTES } from 'shared/routes';
 import Avatar from 'components/Avatar';
-import Box from '@material-ui/core/Box';
 import { getProgressInPercentages } from 'utils';
 
-const useStyles = makeStyles<Theme>(theme => ({
+const useStyles = makeStyles(theme => ({
   carrouselElement: {
-    height: '25rem',
-    width: '25rem',
+    height: '30rem',
+    width: '20rem',
     padding: theme.spacing(2),
     borderRadius: '10%',
     textDecoration: 'none',
-    background: `linear-gradient(${theme.palette.background.default}, ${theme.palette.background.paper})`,
-
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    transition: '1s all ease',
-    transform: 'rotateX(15deg) scale(0.85)',
-    boxShadow: '0px 5px 10px #555',
-    '&:hover': {
-      transform: 'rotate(0deg) scale(1.1) translateY(10px)',
-    },
-  },
-  carrouselElementInfo: {
-    transform: 'scale(1.2)',
-  },
-  carrouselItem: {
-    perspective: '900px',
   },
   carrousel: {
     [theme.breakpoints.down('xs')]: {
@@ -49,46 +31,45 @@ const useStyles = makeStyles<Theme>(theme => ({
 
 const Carrousel: React.FC<{ newbies: Newbie[] }> = ({ newbies }) => {
   const classes = useStyles();
+  const breakpointSm = useMediaQuery<Theme>(theme => theme.breakpoints.down('xs'));
 
   return (
     <Grid
       container
       spacing={2}
-      wrap={'wrap'}
-      justify='center'
+      wrap={breakpointSm ? 'nowrap' : 'wrap'}
       className={classes.carrousel}>
-      {newbies.map(newbie => (
-        <Grid key={newbie.id} item className={classes.carrouselItem}>
-          <Link
-            className={classes.link}
-            to={ROUTES.BUDDY_TASKS_LIST.replace(':newbieId', newbie.id)}>
-            <Paper className={classes.carrouselElement}>
-              <Box className={classes.carrouselElementInfo}>
+      {newbies.map(newbie => {
+        const startDate = new Date(newbie.startDate);
+        return (
+          <Grid key={newbie.id} item>
+            <Link
+              className={classes.link}
+              to={ROUTES.BUDDY_TASKS_LIST.replace(':newbieId', newbie.id)}>
+              <Paper className={classes.carrouselElement}>
                 <Avatar
                   progress={getProgressInPercentages(newbie.tasksInfo.buddyProgress)}
                   name={newbie.name}
                   position={newbie.position || undefined}></Avatar>
-              </Box>
-            </Paper>
-          </Link>
-        </Grid>
-      ))}
+                {newbie.startDate && (
+                  <Box m={2}>
+                    <Typography align={'center'} component='p' variant='body2'>
+                      <Box component='span' fontWeight={'bold'}>
+                        Start Date
+                      </Box>
+                    </Typography>
+                    <Typography align={'center'} component='p' variant='body2'>
+                      {`${startDate.getDate()}-${startDate.getMonth()}-${startDate.getFullYear()}`}
+                    </Typography>
+                  </Box>
+                )}
+              </Paper>
+            </Link>
+          </Grid>
+        );
+      })}
     </Grid>
   );
 };
-
-// const startDate = new Date(newbie.startDate);
-// {newbie.startDate && (
-//   <Box m={2}>
-//     <Typography align={'center'} component='p' variant='body2'>
-//       <Box component='span' fontWeight={'bold'}>
-//         Start Date
-//       </Box>
-//     </Typography>
-//     <Typography align={'center'} component='p' variant='body2'>
-//       {`${startDate.getDate()}-${startDate.getMonth()}-${startDate.getFullYear()}`}
-//     </Typography>
-//   </Box>
-// )}
 
 export default Carrousel;
--- src/components/NewbieSelect/NewbieSelect.tsx
+++ src/components/NewbieSelect/NewbieSelect.tsx
@@ -5,37 +5,25 @@ import Box from '@material-ui/core/Box';
 import { QueryBuddyArgs, Query, Newbie } from 'buddy-app-schema';
 import AuthContext, { AuthContextData } from 'contexts/AuthContext';
 import { NEWBIE_SELECT } from 'graphql/newbie-select.graphql';
-import { makeStyles, Theme } from '@material-ui/core/styles';
 import PlusButton from 'components/PlusButton';
 import Carrousel from 'components/Carrousel';
 import PageContainer from 'components/PageContainer';
 import Header, { MenuTypes } from 'components/Header';
-import MenuContext, { MenuContextData } from 'contexts/MenuContext';
+import MenuContext from 'contexts/MenuContext';
 import NewbieSelectDictionary from './dictionary';
-
-const useStyles = makeStyles<Theme>(theme => ({
-  title: {
-    textAlign: 'center',
-    marginBottom: theme.spacing(5),
-  },
-  carrouselWarpper: {
-    marginBottom: theme.spacing(2),
-  },
-}));
 
 const NewbieSelect: React.FC = () => {
   const { data: AuthData } = useContext<AuthContextData>(AuthContext);
   const { loading, data } = useQuery<Query, QueryBuddyArgs>(NEWBIE_SELECT, {
     variables: { buddyId: AuthData.userId },
   });
-  const { title, carrouselWarpper } = useStyles();
-  const { toggleMenu } = React.useContext<MenuContextData>(MenuContext);
+  const { toggleMenu } = React.useContext(MenuContext);
 
   return (
     <>
       <Header type={MenuTypes.MENU} onButtonClick={toggleMenu} loading={loading} />
-      <PageContainer data-testid='newbie-select-page' backGroundShape>
-        <Box className={title} component='section'>
+      <PageContainer data-testid='newbie-select-page'>
+        <Box marginBottom={5} component='section'>
           <Typography component='h1' variant='h2'>
             {NewbieSelectDictionary.TITLE}
           </Typography>
@@ -44,7 +32,7 @@ const NewbieSelect: React.FC = () => {
           </Typography>
         </Box>
         {data && data.buddy.newbies && (
-          <Box className={carrouselWarpper} component={'section'}>
+          <Box marginBottom={2} component={'section'}>
             <Carrousel newbies={data.buddy.newbies as Newbie[]} />
           </Box>
         )}
