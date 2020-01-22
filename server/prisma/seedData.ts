eb0cbd36ecf973ac5f5e1556de327cefbd46add9
Michael Money <mpieniazek@me.com>
2020-01-22 17:50:44 +0100

    Setup seed file and sample data for Prisma DB

---
+++ server/prisma/seedData.ts
@@ -0,0 +1,52 @@
+export const sampleNewbieTasks = [
+  {
+    title: 'Setup Macbook',
+    description: `If your new MacBook does not have an account set up for you already, ask your on-boarding buddy to create a local admin one for you. You are going to need an Apple ID. (It's up to you whether you use existing accounts or not but probably better keeping work and personal stuff separate).`,
+  },
+  {
+    title: 'Login to Outlook',
+    description: `Once you are given your email address and temporary password, you can check that outlook is working. When you log in for the first time with the default password via <a href="http://webmail.wipro.com/">www.webmail.wipro</a>.com, you'll then be asked to use <a href="https://outlook.com/owa/wipro.com">www.outlook.com/owa/wipro.com</a> in future (and now), where you will change your password. It needs to be between 8 and 16 characters, alphanumeric and at least one special character.`,
+  },
+  {
+    title: 'Create an Outlook signature',
+    description: `Now that we are officially "Buildit@wiprodigital" for now it's best to copy a signature from your buddy. Previously, we used - http://esig.wiprodigital.com`,
+  },
+];
+
+export const sampleBuddyTasks = [
+  {
+    title: 'Meet and Greet a Newbie',
+    description: `Meet Newbie at the reception and conduct site orientation.`,
+  },
+  {
+    title: 'Pick up MacBook for a Newbie',
+    description: `Pick up  MacBook & set up the basics for the Newbie if it's not already done.`,
+  },
+  {
+    title: 'Invite into Slack channels',
+    description: `Send invitations to Slack channels:
+    <ul><li>#buildit,</li><li>#buildit-poland,</li><li>#gtmt,</li><li>#global-backlog-tribe,</li></ul> `,
+  },
+];
+
+export const sampleNewbie = {
+  name: 'John Rambo',
+  email: 'jrambo@wipro.com',
+  password: 'test1234',
+  position: 'Front End Engineer',
+  photo: 'https://i.pravatar.cc/150?img=3',
+  phoneNumber: '+48 642 913 551',
+  startDate: '2019-05-02T11:55:33.488Z',
+  allowPushedNotifications: false,
+};
+
+export const sampleBuddy = {
+  email: '`jwick@wipro.com`',
+  name: 'John Wick',
+  password: 'test1234',
+  position: 'Delivery Lead',
+  photo: 'https://i.pravatar.cc/150?img=11',
+  phoneNumber: '+48 721 588 392',
+  startDate: '2019-10-01T09:36:21.488Z',
+  allowPushedNotifications: false,
+};
