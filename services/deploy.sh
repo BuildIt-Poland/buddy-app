1af1fef4657607cc1f9958d9b314afce29b932c0
Anton Lunov <las12041991@gmail.com>
2020-01-30 23:23:50 +0100

    Update deploy.sh

--- services/deploy.sh
+++ services/deploy.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 cd services
-npm run sls config credentials --provider aws --key $AWS_ACCESS_KEY --secret $AWS_SECRET_ACCESS_KEY
+npm run serverless config credentials --provider aws --key $AWS_ACCESS_KEY --secret $AWS_SECRET_ACCESS_KEY
 npm run deploy --s $1
