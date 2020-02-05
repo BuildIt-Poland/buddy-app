396f89afa0de019a273019e5fc65903b78e74f84
Luciano Santos <luciusjss@gmail.com>
2020-02-05 16:43:53 +0100

    removed video option from cypress to run faster on pipeline

--- src/components/UserDetails/types.ts
+++ src/components/UserDetails/types.ts
@@ -1,5 +1,5 @@
 import { Buddy, Newbie } from 'buddy-app-schema';
 
 export type UserDetailsProps = {
-  details: Partial<Newbie> | Partial<Buddy> & { notes?: string };
+  details: Partial<Newbie> | (Partial<Buddy> & { notes?: string });
 };
