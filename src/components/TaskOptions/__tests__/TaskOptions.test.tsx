03b1b3683639ff2c3948f70403551f4ead13eff7
Tonik1204 <las12041991@gmail.com>
2020-02-12 16:55:23 +0100

    [BUD-270] Add useTaskDelete tests

--- src/components/TaskOptions/__tests__/TaskOptions.test.tsx
+++ src/components/TaskOptions/__tests__/TaskOptions.test.tsx
@@ -1,70 +1,39 @@
 import React from 'react';
-import { render, fireEvent, wait, cleanup } from '@testing-library/react';
+import { create, act } from 'react-test-renderer';
+import waitForExpect from 'wait-for-expect';
+import { MemoryRouter, Route } from 'react-router';
 import { MockedProvider } from '@apollo/react-testing';
 import { mockedBuddyContext } from '__mocks__';
 import { AuthProvider } from 'contexts/AuthContext';
 import { SnackbarProvider } from 'contexts/SnackbarContext';
 import { DialogProvider } from 'contexts/DialogContext';
 import TaskOptions from '../TaskOptions';
-import DICTIONARY from '../dictionary';
 
 describe('Component - TaskOptions', () => {
-  const triggerTaskOptions = (mocks: any) => {
-    const TaskOptionsRoute = render(
-      <MockedProvider resolvers={{}}>
-        <AuthProvider value={mockedBuddyContext()}>
-          <DialogProvider>
-            <SnackbarProvider>
-              <TaskOptions id='1' taskOptionHandlers={mocks} />
-            </SnackbarProvider>
-          </DialogProvider>
-        </AuthProvider>
-      </MockedProvider>
-    );
-    const { getByTestId } = TaskOptionsRoute;
+  const path = '/buddy/newbies/33/tasks';
 
-    const taskOptionsBtn = getByTestId('task-options-btn');
-    fireEvent.click(taskOptionsBtn);
+  const component = create(
+    <MockedProvider>
+      <AuthProvider value={mockedBuddyContext()}>
+        <MemoryRouter initialEntries={[path]}>
+          <Route path={'/buddy/newbies/:newbieId/tasks'}>
+            <DialogProvider>
+              <SnackbarProvider>
+                <TaskOptions id='1' />
+              </SnackbarProvider>
+            </DialogProvider>
+          </Route>
+        </MemoryRouter>
+      </AuthProvider>
+    </MockedProvider>
+  );
 
-    const deleteOption = getByTestId(DICTIONARY.OPTIONS.DELETE);
-    fireEvent.click(deleteOption);
+  it('renders correctly', async () => {
+    expect(component.toJSON()).toMatchSnapshot();
 
-    const deleteDialogConfirmBtn = getByTestId('alert-dialog-confirm');
-    fireEvent.click(deleteDialogConfirmBtn);
-
-    return TaskOptionsRoute;
-  };
-
-  afterEach(() => cleanup);
-
-  describe('when deleting task', () => {
-    describe('when response is success', () => {
-      it('should render success dialog', async () => {
-        const deleteTaskSuccessMock = {
-          deleteTask: () => Promise.resolve(),
-        };
-        const { getByTestId } = triggerTaskOptions(deleteTaskSuccessMock);
-        await wait(() => {
-          expect(getByTestId('snack-bar')).toBeInTheDocument();
-          expect(getByTestId('snack-bar')).toHaveTextContent(
-            DICTIONARY.DELETE_SNACKBAR.SUCCESS
-          );
-        });
-      });
-    });
-
-    describe('when the server throws an error', () => {
-      it('should render error dialog', async () => {
-        const deleteTaskFailedMock = {
-          deleteTask: () => Promise.reject(),
-        };
-        const { getByTestId } = triggerTaskOptions(deleteTaskFailedMock);
-        await wait(() => {
-          expect(getByTestId('snack-bar')).toBeInTheDocument();
-          expect(getByTestId('snack-bar')).toHaveTextContent(
-            DICTIONARY.DELETE_SNACKBAR.ERROR
-          );
-        });
+    await act(async () => {
+      await waitForExpect(() => {
+        expect(component.toJSON()).toMatchSnapshot();
       });
     });
   });
