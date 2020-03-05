43e3c042a385eff0676db7f714398742d95d87d4
Tonik1204 <las12041991@gmail.com>
2020-03-05 12:06:38 +0200

    [BUD-295] Update server deploy.sh

--- packages/client/deploy.sh
+++ packages/client/deploy.sh
@@ -6,5 +6,4 @@ export REACT_APP_SERVER_URL=https://$API_GATEWAY_DOMAIN/$APP_STAGE
 export REACT_APP_BUCKET=$APP_BUCKET-$APP_STAGE
 [ $APP_STAGE = prod ] && export REACT_APP_DOMAIN=$APP_DOMAIN || export REACT_APP_DOMAIN=$APP_STAGE-$APP_DOMAIN
 
-yarn serverless config credentials --provider aws --key $AWS_ACCESS_KEY --secret $AWS_SECRET_ACCESS_KEY
 yarn deploy
--- packages/services/deploy.sh
+++ packages/services/deploy.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 cd packages/services
-yarn sls config credentials --provider aws --key $AWS_ACCESS_KEY --secret $AWS_SECRET_ACCESS_KEY
+yarn sls config credentials --provider aws --key $AWS_ACCESS_KEY_ID --secret $AWS_SECRET_ACCESS_KEY
 yarn sls deploy -s $SERVICES_ENV
