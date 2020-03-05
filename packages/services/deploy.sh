c1848b4a356beab5334d78d01ae3cb025c142540
Tonik1204 <las12041991@gmail.com>
2020-03-05 13:16:33 +0200

    [BUD-295] Update server deploy.sh

--- packages/services/deploy.sh
+++ packages/services/deploy.sh
@@ -2,4 +2,4 @@
 
 cd packages/services
 yarn sls config credentials --provider aws --key $AWS_ACCESS_KEY_ID --secret $AWS_SECRET_ACCESS_KEY
-yarn sls deploy -s $SERVICES_ENV
+yarn sls deploy -s $SERVICES_STAGE
