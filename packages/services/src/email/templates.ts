0953d50e16e98af94fd8664cd9ce5f34688db245
Tonik1204 <las12041991@gmail.com>
2020-04-08 14:30:54 +0200

    [BUD-329] Add expiry information to email description

--- packages/services/src/email/templates.ts
+++ packages/services/src/email/templates.ts
@@ -5,7 +5,8 @@ export const getForgotPasswordTemplate = (name: string, link: string) => ({
       <h1 style="margin-top: 0;">Password reset</h1>
       <img src="https://buddyapp.buildit.digital/favicon-196.png" width="125px" alt="Buddy app logo">
       <h3>Hi ${name},</h3>
-      <p style='margin: 2rem 0;'>You requested to reset your Buddy App password. Click the button below to complete the process.</p>
+      <p style='margin: 2rem 0 0;'>You requested to reset your Buddy App password. For security reasons the link can only be active for one hour.</p>
+      <p style='margin: 0 0 2rem;'>Click the button below to complete the process.</p>
       <a href='${link}' style='background: #007c6c;border-radius: 5rem;color: #f1f1f1!important;display: inline-block;text-decoration:none!important;font-weight:bold;padding: 0.75rem 2.5rem;' target='_blank'>Reset my password</a>
     </div>
   `,
