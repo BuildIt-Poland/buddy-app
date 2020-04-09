a0cfbcfb93af28682ed8275b2f62a7b576516b26
Tonik1204 <las12041991@gmail.com>
2020-04-09 13:04:12 +0200

    Add AWS_REGION env

--- packages/services/deploy.sh
+++ packages/services/deploy.sh
@@ -2,5 +2,5 @@
 
 cd packages/services
 yarn sls config credentials --provider aws --key $AWS_ACCESS_KEY_ID --secret $AWS_SECRET_ACCESS_KEY
-yarn sls remove -s $SERVICES_STAGE
-yarn sls deploy -s $SERVICES_STAGE
+yarn sls remove -s $SERVICES_STAGE -r $AWS_REGION
+yarn sls deploy -s $SERVICES_STAGE -r $AWS_REGION
