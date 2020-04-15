a83cfcb418107f5cb0640dbc197fabdc390a39c7
Tonik1204 <las12041991@gmail.com>
2020-04-15 16:34:03 +0200

    [BUD-326] Add backups aot

--- packages/services/deploy.sh
+++ packages/services/deploy.sh
@@ -2,5 +2,10 @@
 
 cd packages/services
 yarn sls config credentials --provider aws --key $AWS_ACCESS_KEY_ID --secret $AWS_SECRET_ACCESS_KEY
+
 yarn sls remove -s $SERVICES_STAGE -r $AWS_REGION
 yarn sls deploy -s $SERVICES_STAGE -r $AWS_REGION
+
+mkdir -p prisma/backup
+yarn run prisma:backup -p prisma/backup/db-backup.zip
+echo Y | yarn sls client deploy -c prisma/serverless.yml -s $PRISMA_STAGE -r $AWS_REGION
